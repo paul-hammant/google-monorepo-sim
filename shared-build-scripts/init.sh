858802357235dba2f9de8349f1628f7752fbb26a
Paul Hammant <paul@hammant.org>
2025-05-20 08:07:29 +0100

    counts of invocations per script - all above 1st invocation were skipped, so a count of 10 would be

--- shared-build-scripts/init.sh
+++ shared-build-scripts/init.sh
@@ -9,11 +9,16 @@ fi
 if [[ "$REDUNDANTLY_REVISIT_SCRIPTS" == "true" ]]; then
   echo "$relative_script_path: visiting"
 else
-  if ! grep -Fxq "$relative_script_path" "$root/.buildStepsDoneLastExecution"; then
+  if ! grep -q "$relative_script_path" "$root/.buildStepsDoneLastExecution"; then
     #echo "$relative_script_path: visiting"
-    echo "$relative_script_path" >> "$root/.buildStepsDoneLastExecution"
+    if grep -q "$relative_script_path" "$root/.buildStepsDoneLastExecution"; then
+      awk -v path="$relative_script_path" 'BEGIN{FS=OFS=":"} $1==path {$2+=1} 1' "$root/.buildStepsDoneLastExecution" > temp && mv temp "$root/.buildStepsDoneLastExecution"
+    else
+      echo "$relative_script_path:1" >> "$root/.buildStepsDoneLastExecution"
+    fi
   else
     #echo "$relative_script_path: visited already in this execution, no need to do so again"
+    awk -v path="$relative_script_path" 'BEGIN{FS=OFS=":"} $1==path {$2+=1} 1' "$root/.buildStepsDoneLastExecution" > temp && mv temp "$root/.buildStepsDoneLastExecution"
     exit 0
   fi
 fi

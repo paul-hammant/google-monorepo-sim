e10ee18a76ce988368240dff4e763d7568956070
Paul Hammant <paul@hammant.org>
2025-05-31 06:32:56 +0100

    oops

--- .all-tests.sh
+++ .all-tests.sh
@@ -6,6 +6,5 @@ echo "" > "$root/.buildStepsDoneLastExecution"
 echo "Running all .tests.sh in $(pwd) subdirs"
 find . -mindepth 2 -name '.tests.sh' -print0 \
   | while IFS= read -r -d '' script; do
-      echo ">> $script"
       bash "$script" "$root/.buildStepsDoneLastExecution"
     done

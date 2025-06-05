d2d7f665e685d466c9baa57b7f55f70169da908f
Paul Hammant <paul@hammant.org>
2025-06-05 19:14:45 +0100

    rmv debugging

--- shared-build-scripts/add-npm-deps-to-base-tsconfig-json.sh
+++ shared-build-scripts/add-npm-deps-to-base-tsconfig-json.sh
@@ -12,20 +12,16 @@ npm_deps_paths=()
 while IFS= read -r line; do
   IFS=':' read -r key path <<< "$line"
   for npm_dep in "${npm_deps[@]}"; do
-    echo ">k $key .. $npm_dep"
     if [[ "$npm_dep" == "libs:javascript/$key" ]]; then
       npm_deps_paths+=("$key:$root/libs/javascript/npm_vendored/$path")
     fi
   done
 done < "$root/libs/javascript/npm_vendored/package-map"
 
-echo ">> ${npm_deps_paths[@]}"
-
 # Couple npm dependencies with their paths
 declare -A npm_paths_map
 for npm_dep_path in "${npm_deps_paths[@]}"; do
   IFS=':' read -r key value <<< "$npm_dep_path"
-  echo "11 $key .. $value"
   jq --arg key "$key" --arg value "$value" \
     '.compilerOptions.paths[$key] += [$value]' \
     "$root/target/$destination/base-tsconfig.json" | sponge "$root/target/$destination/base-tsconfig.json"
